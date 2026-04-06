99abf3df6fc9315377f6fe070e06f08637ef1faf
Pedro <pedrohgvenancio52@gmail.com>
2026-04-06 11:06:56 -0300

    projeto exercicio

---
+++ aula3003/react-navegacao/src/routes/index.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+
+import Principal from "../pages/principal.css";
+export default function Rotas(){
+    return{
+        <Routes>
+        <Route> </Route>
+        </Routes>
+    }
+}
---
+++ projeto/estacao-metereologica-front/src/pages/login/index.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.css";
+
+export default function Login() {
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+
+  return (
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      
+      <div className="card p-4 shadow" style={{ width: "350px" }}>
+        
+        <h2 className="text-center mb-4">Login</h2>
+
+        <form>
+          
+          {/* USUÁRIO */}
+          <div className="mb-3">
+            <label className="form-label">Usuário</label>
+            <input
+              type="text"
+              className="form-control"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+          </div>
+
+          {/* SENHA */}
+          <div className="mb-3">
+            <label className="form-label">Senha</label>
+            <input
+              type="password"
+              className="form-control"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+          </div>
+
+          {/* BOTÃO */}
+          <button className="btn btn-primary w-100">
+            Entrar
+          </button>
+
+        </form>
+      </div>
+
+    </div>
+  );
+}
